2043a6fe25b7aeb6321a72c5f0f3b1f43863b60d
GlendyT <glendytuyuc@gmail.com>
2024-04-25 17:07:56 -0600

    last changes

--- src/components/base/Button.jsx
+++ src/components/base/Button.jsx
@@ -5,8 +5,8 @@ const Button = ({ variant, className, children, ...rest }) => {
     <button
       type="button"
       className={classNames(className, 'hover:shadow-inner px-4 py-2 text-sm rounded-3xl', {
-        'bg-blue-500 text-white hover:bg-blue-700 hover:text-white': variant === 'primary',
-        'bg-red-500 text-white hover:bg-red-700 hover:text-white': variant === 'secondary',
+        'bg-purple-700 text-white hover:bg-purple-700 hover:text-white': variant === 'primary',
+        'bg-red-700 text-white hover:bg-red-700 hover:text-white': variant === 'secondary',
         'bg-white text-gray-900 hover:bg-white hover:text-blue-500': variant === 'light'
       })}
       {...rest}
--- src/components/base/Modal.jsx
+++ src/components/base/Modal.jsx
@@ -13,12 +13,12 @@ const Modal = ({ open, children }) => {
           <div className="absolute inset-0 bg-gray-900 opacity-75"></div>
           <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
           <div
-            className="inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full"
+            className="inline-block align-center bg-purple-300 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full"
             role="dialog"
             aria-modal="true"
             aria-labelledby="modal-headline"
           >
-            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">{children}</div>
+            <div className="bg-purple-100 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">{children}</div>
           </div>
         </div>
       </div>
